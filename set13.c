cf8da12ead0ba20a2b8bd29f532f81ec1317ad43
NANTAGOPAL S <47105467+NANTAGOPAL3599@users.noreply.github.com>
2019-02-11 16:35:35 +0530

    Update set13.c

--- set13.c
+++ set13.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-char a;
-scanf("%c",&a);
-if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
-   {printf("vowel");}
-else
-printf("consonant");
+	char x;
+	printf("Enter the character:");
+	scanf("%c",&x);
+	if(x=='a'||x=='A'||x=='e'||x=='E'||x=='i'||x=='I'||x=='o'||x=='O'||x=='u'||x=='U')
+	{
+		printf("vowel");
+	}
+	else
+	{
+		printf("consonant");
+	}
 }
