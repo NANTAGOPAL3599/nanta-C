676c4a8318efd8be422eb56a57cbc1dbb5d50c19
NANTAGOPAL S <47105467+NANTAGOPAL3599@users.noreply.github.com>
2019-02-20 16:20:32 +0530

    Create set2-3.c

---
+++ set2-3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,i,temp=0;
+printf("Enter an integer:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+temp++;
+
+}}
+if(temp==2)
+{
+printf("the num is prime",n);
+}
+else
+{
+printf("the num is not prime",n);
+}
+}
